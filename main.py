61cc850736270fa060399c67a64f70f4fb86ffaf
vlzcv <vlada.zaytseva.2009@mail.ru>
2024-11-08 16:53:42 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+hero = input("Ваш персонаж супергерой?: ")
+if hero == True:
+    print("Ваш персонаж является супергероем")
+if hero == False:
+    print("Ваш персонаж не является супергероем")
+name = input("Введите имя ващего персанажа: ")
+age = input("Введите возраст вашего персонажа: ")
+age = int(age)
+new_age = input("Введите новый возраст вашего персонажа: ")
+new_age = int(new_age)
+print(name, age, age + new_age)
+
+
+
+
+
